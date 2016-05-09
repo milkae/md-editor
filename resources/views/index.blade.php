c3bff4d990a81b4470c24987c1db577a5317fee1
Floriane <floriane_grondin@hotmail.com>
2016-05-09 16:23:29 +0200

    Mise en place base Laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="codemirror/lib/codemirror.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Editeur de Markdown</title>
+</head>
+<body>
+    <section id="main"></section>
+    <script src="codemirror/lib/codemirror.js"></script>
+    <script src="codemirror/mode/markdown/markdown.js"></script>
+    <script src="/js/marked.min.js"></script>
+    <script src="/js/bundle.js"></script>
+</body>
+</html>
